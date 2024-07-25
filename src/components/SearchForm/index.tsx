2b863e1c0a099f7edd8e9321afe3605fe1120be1
Lucas Farias <lucas.farias@rtbhouse.com>
2024-07-25 20:58:04 -0300

    add axios dep and implement fetchTransactions

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,7 +1,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { MagnifyingGlass } from "phosphor-react";
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import * as zod from "zod";
+import { TransactionsContext } from "../../contexts/transactions";
 import { SearchFormContainer } from "./styles";
 
 const searchFormSchema = zod.object({
@@ -11,6 +13,8 @@ const searchFormSchema = zod.object({
 type SearchFormInputs = zod.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext);
+
   const {
     register,
     handleSubmit,
@@ -20,9 +24,7 @@ export function SearchForm() {
   });
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    console.log(data);
+    await fetchTransactions(data.query);
   }
 
   return (
